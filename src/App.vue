fdaf8f4eac8bec063ad6828d509a4bc52e8d6362
jemshMarcel <jemshjms@gmaill.com>
2024-05-29 16:04:27 +0300

    computed properties lesson

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { computed, ref } from 'vue';
 
 
 
   const product = ref('Socks');
+  const brand = ref('Vue Mastery');
   let cart = ref(0);
-  let image = ref('../public/images/socks_blue.jpg');
-  const inStock = ref(false);
+  // let image = ref('../public/images/socks_blue.jpg');
+  let selectedVariant = ref(0);
+  // const inStock = ref(false);
   const details = ['50% cotton', '30% wool', '20% polyester'];
   const variants = [
-    {id: 2234, color: 'green',image:'../public/images/socks_green.jpg'},
-    {id: 2235, color: 'blue',image:'../public/images/socks_blue.jpg'},
+    {id: 2234, color: 'green',image:'../public/images/socks_green.jpg', quantity:50},
+    {id: 2235, color: 'blue',image:'../public/images/socks_blue.jpg', quantity:0},
   ];
 
   function addToCart(){
     cart.value ++;
   }
-  function updateImage(variantImage){
-    image.value = variantImage;
+  function updateVariant(index){
+    selectedVariant.value = index;
   }
+
+  const title = computed(()=>{
+    return brand.value + ' ' + product.value;
+  });
+  const image = computed(()=>{
+    return variants[selectedVariant.value].image
+  })
+  const inStock = computed(()=>{
+    return variants[selectedVariant.value].quantity;
+  })
   
   
   
@@ -34,17 +46,19 @@ import { onMounted, ref } from 'vue';
           <img :src="image">
         </div>
         <div class="product-info">
-          <h1>{{ product }}</h1>
+          <h1>{{ title }}</h1>
+          
           <p v-if="inStock">In Stock</p> 
           <p v-else >Out of Stock</p> 
+
           <ul>
             <li v-for="detail in details">{{ detail }}</li>
           </ul>
 
           <div 
-            v-for="variant in variants" 
+            v-for="variant, index in variants"
             :key="variant.id"
-            @mouseover="updateImage(variant.image)"
+            @mouseover="updateVariant(index)"
             class="color-circle"
             :style="{ backgroundColor:variant.color }">
           </div>
